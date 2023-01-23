3bad8e4875821e1a27a8dbc6d2f585648a565568
Tenongene <tenongene00@gmail.com>
2023-01-22 20:56:08 -0600

    Refactoring for persistence

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ app.set('view engine', 'ejs');
 //initialize redis-db
 const client = redis.createClient();
 client.connect();
-let count = 0;
 
 //defining get route
 app.get('/',  (req, res) => {
-  client.set('visit_count', JSON.stringify(count++));
-  let visit_count = client.get('visit_count')
-  .then(result => res.render('index', {count: result}))
+  client.get('visit_count')
+  .then(result => {
+    result++;
+    res.render('index', {count: result});
+    client.set('visit_count', result)
+  })
   .catch(err => console.log(err.message));
 })
